027bf42c05160d19c8eb9f3826ee3edccb451bb9
Felix Fong <moongod101@hotmail.com>
2019-09-28 16:58:41 +0800

    Updated test case to cover the new API

--- test/Methods/User.test.js
+++ test/Methods/User.test.js
@@ -1,15 +1,37 @@
 const KB = require('../../src/index');
 const USERNAME = 'felixfong227';
 
-test('Should return a person username', async done => {
-    const them = await KB().User().GetUser(USERNAME)
+test('Should return one single person username', async done => {
+    const person = await KB().User().GetUser().ByName(USERNAME);
+    expect(person.basics.username).toBe(USERNAME);
+    done();
+});
+
+test('Should return one single person username', async done => {
+    const them = await KB().User().GetUser().ByNames(
+        [
+            USERNAME,
+            'owo'
+        ]
+    );
     expect(them[0].basics.username).toBe(USERNAME);
+    expect(them[1].basics.username).toBe('owo');
     done();
 });
 
 test('Should return a person public GPG key', async done => {
     const personPGPKey = await KB().User().GetPGPKey(USERNAME);
     const firstBlock = personPGPKey[0];
-    if(firstBlock.startsWith('-----BEGIN PGP PUBLIC KEY BLOCK-----')) return done();
-    throw new Error('Response did not contain public key');
+    expect(firstBlock).toMatch(
+        /^-----BEGIN PGP PUBLIC KEY BLOCK-----/
+    );
+    done();
+});
+
+test('Should create a valid login session between this machine and Keybase server', async done => {
+    await KB().Login({
+        username: 'felixfong227',
+        secretKeyPath: './private.asc'
+    });
+    return done();
 });
